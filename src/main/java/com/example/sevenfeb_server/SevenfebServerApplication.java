7a794338aad8febc186763e56ba617477542de2d
JangHwanPark <wkd5878@gmail.com>
2024-03-08 22:19:24 +0900

    init

---
+++ src/main/java/com/example/sevenfeb_server/SevenfebServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.sevenfeb_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SevenfebServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SevenfebServerApplication.class, args);
+    }
+
+}
